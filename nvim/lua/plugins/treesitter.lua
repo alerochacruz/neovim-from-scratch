f1b78dd2d9e6a116325ad2549ab5df1423ff7d34
Alejandro Rocha Cruz <alerochacruz@aol.com>
2024-02-14 02:54:46 -0300

    Swap tabs for spaces

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,21 +5,21 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-				ensure_installed = {
-					"javascript",
+        ensure_installed = {
+          "javascript",
           "python",
-					"tsx",
-					"toml",
-					"php",
-					"json",
-					"yaml",
-					"css",
-					"html",
-					"lua"
-				},
-				autotag = {
-					enable = true,
-				},
+          "tsx",
+          "toml",
+          "php",
+          "json",
+          "yaml",
+          "css",
+          "html",
+          "lua"
+        },
+        autotag = {
+          enable = true,
+        },
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },

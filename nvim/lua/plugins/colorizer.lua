8053a5648e3dff8be0be352a6e9876957208ffd4
Alejandro Rocha Cruz <alerochacruz@aol.com>
2024-02-17 08:59:32 -0300

    Add colorlizer.lua options

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -2,7 +2,14 @@ return {
   {
     "norcalli/nvim-colorizer.lua",
     config = function()
-      require("colorizer").setup()
+      require("colorizer").setup({
+        "css",
+        "html",
+        "javascript",
+        css = { css = true },
+        html = { css = true },
+        javascript = { css = true },
+      })
     end,
   },
 }
